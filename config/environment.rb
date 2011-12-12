8ab858bb685ca508fdcb5c5d80281f60ba05892d
Dario Seminara <dario@bandofcoders.com>
2011-12-12 16:24:40 -0300

    added certificate to connect with google identity provider

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 #OpenIdAuthentication.store = :file
 
+OpenID.fetcher.ca_file = "#{Rails.root}/config/ca-bundle.crt"
+
 # Initialize the rails application
 OpenidTest::Application.initialize!
