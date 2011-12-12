3205ef61c3b9417d4e683950d04701d4ba3a8771
Dario Seminara <dario@bandofcoders.com>
2011-12-12 14:34:56 -0300

    added register screen

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,13 @@
   class SessionsController < ApplicationController
     def create
       if using_open_id?("https://www.google.com/accounts/o8/id")
-        open_id_authentication
+        open_id_authentication(:register_username => params[:register_username])
       else
         failed_login "Sorry, only login with openid are accepted"
       end
+    end
+    
+    def register
     end
     
     def failed
@@ -16,13 +19,20 @@
     end
 
     protected
-      def open_id_authentication
+      def open_id_authentication(args)
         authenticate_with_open_id("https://www.google.com/accounts/o8/id") do |result, identity_url|
           if result.successful?
-            if @current_user = User.find_by_identity_url(identity_url)
+            @current_user = User.find_by_identity_url(identity_url)
+            
+            if @current_user
               successful_login
             else
-              failed_login "Sorry, no user by that identity URL exists (#{identity_url})"
+              if args[:register_username]
+                @current_user = User.new(:identity_url => identity_url, :username => args[:register_username])
+                successful_login
+              else
+                redirect_to '/sessions/register'
+              end
             end
           else
             failed_login result.message
