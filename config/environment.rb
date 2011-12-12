737eb8994c7b8030497255129763f27708835940
Dario Seminara <dario@bandofcoders.com>
2011-12-12 12:29:28 -0300

    removed store option filesytem

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-OpenIdAuthentication.store = :file
+#OpenIdAuthentication.store = :file
 
 # Initialize the rails application
 OpenidTest::Application.initialize!
