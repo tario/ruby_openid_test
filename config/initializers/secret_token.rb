3903a6c0afa355e071e0d4e85a4aac1e64ce9bce
Dario Seminara <dario@bandofcoders.com>
2011-12-12 11:46:47 -0300

    Rails 3.1.3 project created from scratch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenidTest::Application.config.secret_token = 'b5c7bc19945abe8c34c8924390b5fbe199aa0074edd90ac7132fb83687ccbc6997244daf45df607277adfd1b661d3c455acf008db7ca226eaf8ff82a454d54f9'
